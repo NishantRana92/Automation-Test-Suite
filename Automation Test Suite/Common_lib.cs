826b5be4a54d89b39e25895b5c99c2e5a24b1e49
Nishant Rana <Nishant.Rana@rezare.com>
2022-01-24 08:54:24 +1300

    Adding all files

---
+++ Automation Test Suite/Common_lib.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace Automation_Test_Suite
+{
+    public class Common_lib : SetupClass
+    {
+        public bool Verify_elemnt(string elementName)
+        {
+
+            try
+            {
+                bool isElementDisplayed = driver.FindElement(By.Id(elementName)).Displayed;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public void Adding_mandatory_fields(string firstname, string email, string message)
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("forename"))).SendKeys(firstname);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("email"))).SendKeys(email);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("message"))).SendKeys(message);
+        }
+
+        public void Adding_products_to_cart(int x, string element_id)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id(element_id))).FindElement(By.ClassName("btn")).Click();
+            }
+
+        }
+    }
+}
---
+++ Automation Test Suite/SetupClass.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium.Support.UI;
+
+namespace Automation_Test_Suite
+{
+    [TestFixture]
+    public class SetupClass
+    {
+        protected IWebDriver driver;
+
+        protected WebDriverWait wait;
+
+        [SetUp]
+        public void OneTimeSetUp()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--window-size=1920,1080");
+            chromeOptions.AddArguments("--start-maximized");
+            chromeOptions.AddArguments("--headless");
+            chromeOptions.AddArgument("no-sandbox");
+            chromeOptions.AddArguments("--disable-gpu");
+            chromeOptions.AddArguments("--disable-extensions");
+            driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://jupiter.cloud.planittesting.com");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        
+        [TearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
